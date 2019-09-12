3f168728b8dfb89612f07a40cc48d952a333482b
bl1n <sukhanov87@gmail.com>
2019-09-12 11:01:20 +0300

    1

--- app/src/main/java/team/lf/task3/MainActivity.java
+++ app/src/main/java/team/lf/task3/MainActivity.java
@@ -1,57 +1,30 @@
 package team.lf.task3;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.Toast;
 
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.android.material.navigation.NavigationView;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.bottomappbar.BottomAppBar;
 
 public class MainActivity extends AppCompatActivity {
 
-    BottomNavigationView.OnNavigationItemSelectedListener mItemSelectedListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-            switch (menuItem.getItemId()){
-                case R.id.app_bar_search:{
-                    Toast.makeText(MainActivity.this, "Search", Toast.LENGTH_SHORT).show();
-                    return true;
-                }
-                case R.id.app_bar_fav:{
-                    Toast.makeText(MainActivity.this, "Favorite", Toast.LENGTH_SHORT).show();
-                    return true;
-                }
-                case R.id.app_bar_settings:{
-                    Toast.makeText(MainActivity.this, "Settings", Toast.LENGTH_SHORT).show();
-                    return true;
-                }
-                case R.id.app_bar_trip:{
-                    Toast.makeText(MainActivity.this, "Trip", Toast.LENGTH_SHORT).show();
-                    return true;
-                }
-            }
-            return false;
-        }
-    };
+
+    BottomAppBar mBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        BottomNavigationView navigation = findViewById(R.id.navigation);
-
-        navigation.setOnNavigationItemSelectedListener(mItemSelectedListener);
+//        mBar = findViewById(R.id.bar);
+//       setSupportActionBar(mBar);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
+//        inflater.inflate(R.menu.bar_menu, menu);
         return true;
     }
 }
