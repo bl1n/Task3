b6e830528b63d659ede3ab58b5a38834489d21c1
bl1n <sukhanov87@gmail.com>
2019-09-12 11:06:24 +0300

    1

--- app/src/main/java/team/lf/task3/MainActivity.java
+++ app/src/main/java/team/lf/task3/MainActivity.java
@@ -3,28 +3,36 @@ package team.lf.task3;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.android.material.bottomappbar.BottomAppBar;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
 
-    BottomAppBar mBar;
+
+    private BottomNavigationView mNavigation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        mBar = findViewById(R.id.bar);
-//       setSupportActionBar(mBar);
+        mNavigation = findViewById(R.id.navigation);
+
+        mNavigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                Toast.makeText(MainActivity.this, menuItem.getTitle(), Toast.LENGTH_SHORT).show();
+                return true;
+            }
+        });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-//        inflater.inflate(R.menu.bar_menu, menu);
-        return true;
-    }
+
+
+
 }
